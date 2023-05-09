deb5db8de2fa5140c30c1c5a9cf3ae420aa3bf2c
maxlestage <maxlestage@icloud.com>
2023-05-10 01:40:34 +0200

    getAll ok

--- src/components/videos/GetAllData.jsx
+++ src/components/videos/GetAllData.jsx
@@ -1,27 +1,48 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Row, Card, Col } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import { getAllData } from "../../services/dataServices";
+
 function GetAllData() {
   const [posts, setPosts] = useState([]);
-  /// pour chaque nom verifier "&#039;s &quot;" et si présent le sortir.
+
   useEffect(() => {
     getAllData().then((response) => {
       setPosts(response.data);
     });
   }, []);
 
+  console.log("Posts", posts);
+
   return (
-    <div className="card-container">
-      {posts
-        .sort(({ id: previousID }, { id: currentID }) => currentID - previousID)
-        .map((post, i) => (
-          <div key={post.video_name + post.link + i++}>
-            <h2>{post.video_name}</h2>
-            <p>{post.description}</p>
-            <p>{post.link}</p>
-          </div>
-        ))}
-    </div>
+    <Row xs={2} md={3} lg={4} className="g-4 justify-content-md-center my-5">
+      {posts.slice(0).map((post, idx) => (
+        <Col key={idx} className="mx-4">
+          <Card border="dark" className="mb-2" style={{ height: "100%" }}>
+            <Card.Header className="text-center">
+              {post.video_name
+                .replace(/&#039; &quot;/g, " ")
+                .replace(/&quot;/, " ")
+                .replace(/&#039;/, " ")
+                .replace(/&quot;/, " ")}
+            </Card.Header>
+            <Card.Body>
+              <Card.Title
+                style={{
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  maxWidth: "100%",
+                }}
+              >
+                {post.description}
+              </Card.Title>
+              <Card.Link>{post.link}</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   );
 }
 
